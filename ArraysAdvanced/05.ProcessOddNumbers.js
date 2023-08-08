bc94972b2c8caea4e3d5ecb2e920a74de7142eb7
PlamenaKaloyanova <114017277+PlamenaKaloyanova@users.noreply.github.com>
2023-08-08 16:54:16 +0300

    Create 05.ProcessOddNumbers.js

---
+++ ArraysAdvanced/05.ProcessOddNumbers.js
@@ -0,0 +1,9 @@
+function oddNumberProcessor(numbers) {
+    // method chaining
+    let result = numbers
+        .filter((x, i) => i % 2 !== 0)
+        .map(x => x * 2)
+        .reverse();
+
+    console.log(result.join(' '));
+}
