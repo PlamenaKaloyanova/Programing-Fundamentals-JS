caddc5a588ebb15b70de1a5c976c663bad9b972e
PlamenaKaloyanova <114017277+PlamenaKaloyanova@users.noreply.github.com>
2023-08-08 16:53:49 +0300

    Create 04. LastKNumbersSequance.js

---
+++ ArraysAdvanced/04. LastKNumbersSequance.js
@@ -0,0 +1,18 @@
+function lastKnumbersSequence(length,k){
+
+    let sequence = [1];
+    
+    for (let i = 1; i < length; i++) {
+        let index = Math.max(sequence.length - k, 0);
+
+        let lastElements = sequence.slice(index);
+        let sum = 0;
+        for (const el of lastElements) {
+            sum += el;
+        }
+
+        sequence.push(sum);
+    }
+
+    console.log(sequence.join(' '));
+}
