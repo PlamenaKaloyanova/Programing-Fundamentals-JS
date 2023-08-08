9f01bae48c4bfd6b32fea48e5e0c9d95e803160e
PlamenaKaloyanova <114017277+PlamenaKaloyanova@users.noreply.github.com>
2023-08-08 15:14:19 +0300

    Create 02.CommonElements.js

---
+++ Arrays/02.CommonElements.js
@@ -0,0 +1,17 @@
+function demo(arrayOne,arrayTwo) {
+ 
+
+    for(let i=0;i<arrayOne.length;i++){
+      
+    
+    for(let k=0;k<arrayTwo.length;k++){
+
+        if(arrayOne[i]===arrayTwo[k]){
+            console.log(arrayOne[i])
+        }else{
+            
+        }
+    }
+}
+
+}
