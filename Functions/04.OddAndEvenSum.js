2f0a3e858102d0d61128c94e6ec4df12c883500f
PlamenaKaloyanova <114017277+PlamenaKaloyanova@users.noreply.github.com>
2023-08-08 16:43:56 +0300

    Create 04.OddAndEvenSum.js

---
+++ Functions/04.OddAndEvenSum.js
@@ -0,0 +1,16 @@
+function demo(number){
+
+    let numberString=number.toString();
+    let evenSum=0;
+    let oddSum=0;
+
+    for(let i=0;i<numberString.length;i++){
+        if(numberString[i]%2===0){
+            evenSum+=Number(numberString[i])
+        }else{
+            oddSum+=Number(numberString[i])
+        }
+    }
+ 
+console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`)
+}
